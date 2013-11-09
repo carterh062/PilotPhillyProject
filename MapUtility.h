de990934c3bb2a7bf92beb48c5607459f571a501
Andrii Dobroshynsky <andrii.dobro@gmail.com>
2013-11-09 18:20:41 -0500

    added MapUtility Class and some code to get the latitude and longitude

---
+++ MapUtility.h
@@ -0,0 +1,16 @@
+//
+//  MapUtility.h
+//  ParseSetup
+//
+//  Created by Andrew Dobroshynsky on 11/9/13.
+//  Copyright (c) 2013 Andrew Dobroshynsky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapUtility : NSObject
+
+@property (nonatomic) double latitude;
+@property (nonatomic) double longitude;
+
+@end
--- ViewController.m
+++ ViewController.m
@@ -8,11 +8,14 @@
 
 #import "ViewController.h"
 #import "LocationListViewController.h"
+#import "MapUtility.h"
 
 @interface ViewController ()
+
 @end
 
 @implementation ViewController
+
 
 
 - (void)viewDidLoad
@@ -25,7 +28,25 @@
 - (IBAction)buttonClicked:(id)sender {
     NSLog(@"Button Click!");
     
+    MapUtility *_mapData = [[MapUtility alloc] init];
+    
+    _mapData.latitude = [self.mapView userLocation].location.coordinate.latitude;
+    _mapData.longitude = [self.mapView userLocation].location.coordinate.longitude;
+    
+    NSLog(@"Latitude: %f",[self.mapView userLocation].location.coordinate.latitude);
+    NSLog(@"Longitude: %f",[self.mapView userLocation].location.coordinate.longitude);
+    
+    NSLog(@" %@", _mapData);
+    
+    _mapData.latitude = [self.mapView userLocation].location.coordinate.latitude;
+    _mapData.longitude = [self.mapView userLocation].location.coordinate.longitude;
+    
+    
+    NSLog(@" %f %f", _mapData.longitude, _mapData.latitude);
     LocationListViewController *listViewController = [[LocationListViewController alloc] initWithStyle:UITableViewStylePlain];
+    
+    
+
     [self.navigationController pushViewController:listViewController animated:YES];
 }
 
