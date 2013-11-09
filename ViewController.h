e4ea96d8f9ccea920c8d27056e5b0228e2e34043
Carter Henderson <carterh062@gmail.com>
2013-11-09 15:28:02 -0500

    first commit

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParseSetup
+//
+//  Created by A on 11/9/13.
+//  Copyright (c) 2013 CarterIncorporated. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
+
+@end
